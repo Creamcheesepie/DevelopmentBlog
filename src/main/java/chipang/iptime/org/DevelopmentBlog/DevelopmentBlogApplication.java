89aca88644d991d2f0dab21ca63835f2d97d275c
chipang <gnldbs1004@naver.com>
2024-10-11 11:20:29 +0900

    first commit

---
+++ src/main/java/chipang/iptime/org/DevelopmentBlog/DevelopmentBlogApplication.java
@@ -0,0 +1,13 @@
+package chipang.iptime.org.DevelopmentBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chipang/iptime/org/DevelopmentBlog/DevelopmentBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package chipang.iptime.org.DevelopmentBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
